ac6703bb1af5579b0a883e9608fc7dc5ecf6af6b
chat801fbm <chat801fbm@gmail.com>
2021-11-22 20:15:15 -0600

    Cambios nuevos

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyC3Si67uHAywfuBCGVuGxYJACGO0ALYoUU",
+  authDomain: "chat801fbm-7d922.firebaseapp.com",
+  projectId: "chat801fbm-7d922",
+  storageBucket: "chat801fbm-7d922.appspot.com",
+  messagingSenderId: "957240172179",
+  appId: "1:957240172179:web:0cbb5139c61146e017b766",
+  measurementId: "G-186YBD80V2"    
+    });
